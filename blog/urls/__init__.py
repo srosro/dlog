be7e0d75e07ec4c4e22ea168c2a56bcc6a3eff14
Sam Odio <sam@odio.com>
2011-11-28 15:47:46 -0800

    move topic urls to top-level

--- blog/urls/__init__.py
+++ blog/urls/__init__.py
@@ -2,12 +2,15 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-                       url(r'^topics/', include('blog.urls.tags',)),
                        url(r'^feeds/', include('blog.urls.feeds')),
                        url(r'^authors/', include('blog.urls.authors')),
                        url(r'^search/', include('blog.urls.search')),
                        url(r'^sitemap/', include('blog.urls.sitemap')),
                        url(r'^comments/', include('django.contrib.comments.urls')),
                        url(r'^', include('blog.urls.entries')),
+
+                       #Make tags top-level.  NOTE: this means they might conflict with
+                       #blog articles, which are in the same namespace.
+                       url(r'^', include('blog.urls.tags',)),
                        )
 
